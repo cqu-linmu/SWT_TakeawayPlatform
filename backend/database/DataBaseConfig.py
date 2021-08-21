b892be13745b7bb52e81818403190de6b94e79b5
Aression <1040901628@qq.com>
2021-08-21 17:56:53 +0800

    调整结构

--- backend/database/DataBaseConfig.py
+++
@@ -1,8 +0,0 @@
-DEBUG = True
-SQLALCHEMY_ECHO = True
-#username = root
-#password = 123456
-#SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT,DRIVER,USERNAME,PASSWORD,HOST,PORT,DATABASE)
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@localhost/testdb"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ENCODING = "utf8"
