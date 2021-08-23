cce19f91a7cab7fa855315084b4f30a565e2523d
handsomefan1 <2642413273@qq.com>
2021-08-23 15:13:35 +0800

    updata upload

--- backend/utils/UploadService.py
+++ backend/utils/UploadService.py
@@ -1,41 +1,36 @@
 # -*- coding: utf-8 -*-
 from werkzeug.utils import secure_filename
-from application import app,db
+from application import app, db
 from common.libs.Helper import getCurrentDate
 import datetime
-import  os,stat,uuid
+import os, stat, uuid
 
 
 class UploadService():
-	@staticmethod
-	def uploadByFile( file ):
-		config_upload = app.config['UPLOAD']
-		resp = { 'code':200,'msg':'操作成功~~','data':{} }
-		filename = secure_filename( file.filename )
-		ext = filename.rsplit(".",1)[1]
-		if ext not in config_upload['ext']:
-			resp['code'] = -1
-			resp['msg'] = "不允许的扩展类型文件"
-			return resp
+    @staticmethod
+    def uploadByFile(file):
+        config_upload = app.config['UPLOAD']
+        resp = {'code': 200, 'msg': '操作成功~~', 'data': {}}
+        filename = secure_filename(file.filename)
+        ext = filename.rsplit(".", 1)[1]
+        if ext not in config_upload['ext']:
+            resp['code'] = -1
+            resp['msg'] = "不允许的扩展类型文件"
+            return resp
 
-		root_path = app.root_path + config_upload['prefix_path']
-		# 不使用getCurrentDate创建目录，为了保证其他写的可以用，这里改掉，服务器上好像对时间不兼容
-		file_dir = datetime.datetime.now().strftime("%Y%m%d")
-		save_dir = root_path + file_dir
-		if not os.path.exists( save_dir ):
-			os.mkdir( save_dir )
-			os.chmod( save_dir,stat.S_IRWXU | stat.S_IRGRP |  stat.S_IRWXO )
+        root_path = app.root_path + config_upload['prefix_path']
+        # 不使用getCurrentDate创建目录，为了保证其他写的可以用，这里改掉，服务器上好像对时间不兼容
+        file_dir = datetime.datetime.now().strftime("%Y%m%d")
+        save_dir = root_path + file_dir  # 上传图片的存储路径
+        if not os.path.exists(save_dir):  # 文件夹不存在：创建文件夹
+            os.mkdir(save_dir)
+            os.chmod(save_dir, stat.S_IRWXU | stat.S_IRGRP | stat.S_IRWXO)
 
-		file_name = str( uuid.uuid4() ).replace("-","") + "." + ext
-		file.save( "{0}/{1}".format( save_dir,file_name ) )
+        file_name = str(uuid.uuid4()).replace("-", "") + "." + ext  # 利用uuid（引用的）生成文件名
+        file.save("{0}/{1}".format(save_dir, file_name))  # 存储文件
 
-		model_image = Image()
-		model_image.file_key = file_dir + "/" + file_name
-		model_image.created_time = getCurrentDate()
-		db.session.add( model_image)
-		db.session.commit()
-
-		resp['data'] = {
-			'file_key': model_image.file_key
-		}
-		return resp
+        # 返回信息
+        resp['data'] = {
+            'file_key': file_dir + "/" + file_name
+        }
+        return resp
