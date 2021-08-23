8bbae1b066880dae5bd2b6d2704706dcdf93a04d
handsomefan1 <2642413273@qq.com>
2021-08-23 19:59:15 +0800

    更改user.py & food.py

--- backend/api/food/Food.py
+++ backend/api/food/Food.py
@@ -5,10 +5,10 @@ from application import app, db
 from utils.UrlManager import UrlManager
 from decimal import Decimal
 from sqlalchemy import or_
-
-# 相关数据库
-# from common.models.Food import Dishdatum
-from DataBaseFolder.Models.RestaurantModels.DishBase import Dish
+# 相关数据库调用
+from DataBaseFolder.Interface import DishBaseModify as d
+from DataBaseFolder.Interface import RestaurantBaseModify as r
+from DataBaseFolder.Interface.InterfaceHelper import *
 
 route_food = Blueprint('food_page', __name__)
 
@@ -16,132 +16,88 @@ route_food = Blueprint('food_page', __name__)
 # 餐品管理首页显示
 @route_food.route("/index")
 def index():
-    resp_data = {}  # 返回数据
-
     req = request.values
-    query = Dish.query
-
-    # 利用前端传来'mix_kw'进行搜索（名称或许要修改）
-    # 根据菜名DishName搜索
-    if 'mix_kw' in req:
-        rule = or_(Dish.DishName.ilike("%{0}%".format(req['mix_kw'])))
-        query = query.filter(rule)
-
-    # 分页
     page = int(req['page']) if ('page' in req and req['page']) else 1
     page_size = req['pageSize']
-    pages = db.session.quary(Dish).filter(Dish.DishID.panginate(page, page_size))
+    list = d.PyList()
+    listNum = len(list)
+    pageNum = listNum / page_size
+    lic = []
+    lic.append({"pageNum": pageNum})
 
-    resp_data['list'] = list
-    resp_data['pages'] = pages
-    resp_data['search_con'] = req
+    def bedic(a):
+        for item in a:
+            lic.append(
+                {
+                    "dish_id": item.DishID,
+                    "dish_name": item.DishName,
+                    "dish_class": item.DishType
 
-    resp_data['current'] = 'index'
-    return ops_render("food/index.html", resp_data)
+                }
+            )
+        return lic
 
-
-# 餐品的具体信息显示
-@route_food.route("/info")
-def info():
-    resp_data = {}  # 返回信息
-    req = request.args
-    DishID = int(req.get("DishID", 0))  # 获取菜品编号，根据菜品编号获取菜品的全部信息
-    reback_url = UrlManager.buildUrl("/food/index")  # 跳转的网址
-
-    if DishID < 1:
-        return redirect(reback_url)
-
-    info = Dish.query.filter_by(DishID=DishID).first()
-    if not info:
-        return redirect(reback_url)
-
-    resp_data['info'] = info  # 返回菜品信息
-    resp_data['current'] = 'index'
-    return ops_render("food/info.html", resp_data)
+    return jsonify(bedic(list))
 
 
 # 餐品编辑
 @route_food.route("/set", methods=['GET', 'POST'])
 def set():
-    # 当前端请求为GET，显示菜品信息
-    if request.method == "GET":
-        resp_data = {}
-        req = request.args
-        DishID = int(req.get('DishID', 0))
-        info = Dish.query.filter_by(DishID=DishID).first()
-        resp_data['info'] = info
-        resp_data['current'] = 'index'
-        return ops_render("food/set.html", resp_data)
-
-    resp = {'code': 200, 'msg': '操作成功~~', 'data': {}}  # 提前定义返回信息
+    resp = {'code': 1, 'msg': '编辑成功', 'data': {}}  # 提前定义返回信息
     req = request.values
 
     # 修改数据中菜品的信息
-    DishID = int(req['DishID']) if 'DishID' in req and req['DishID'] else 0  # 菜品编号
     dish_class = req['dish_class'] if 'dish_class' in req else ''  # 菜品分类
     dish_name = req['dish_name'] if 'dish_name' in req else ''  # 菜品名
     dish_price = req['dish_price'] if 'dish_price' in req else ''  # 菜品价格
-    Details_Picture = req['Details_Picture'] if 'Details_Picture' in req else ''  # 菜品图片
     dish_description = req['dish_description'] if 'dish_description' in req else ''  # 菜品描述
-    DishTag = req['DishTag'] if 'DishTag' in req else ''  # 菜品标签
 
     # 检查菜品分类是否合理
     if dish_class is None or len(dish_class) < 1:
-        resp['code'] = -1
+        resp['code'] = 0
         resp['msg'] = "请输入符合规范的分类~~"
         return jsonify(resp)
 
     # 检查菜品名是否合理
     if dish_name is None or len(dish_name) < 1:
-        resp['code'] = -1
+        resp['code'] = 0
         resp['msg'] = "请输入符合规范的名称~~"
         return jsonify(resp)
 
     # 检查菜品价格是否合理
     if not dish_price or len(dish_price) < 1:
-        resp['code'] = -1
+        resp['code'] = 0
         resp['msg'] = "请输入符合规范的售卖价格~~"
         return jsonify(resp)
 
     price = Decimal(dish_price).quantize(Decimal('0.00'))
     if price <= 0:
-        resp['code'] = -1
+        resp['code'] = 0
         resp['msg'] = "请输入符合规范的售卖价格~~"
-        return jsonify(resp)
-
-    # 检查菜品图片是否合理
-    if Details_Picture is None or len(Details_Picture) < 3:
-        resp['code'] = -1
-        resp['msg'] = "请上传封面图~~"
         return jsonify(resp)
 
     # 检查菜品描述是否合理
     if dish_description is None or len(dish_description) < 3:
-        resp['code'] = -1
-        resp['msg'] = "请输入图书描述，并不能少于10个字符~~"
+        resp['code'] = 0
+        resp['msg'] = "请输入描述，并不能少于10个字符~~"
         return jsonify(resp)
 
-    # 检查菜品标签是否合理
-    if DishTag is None or len(DishTag) < 1:
-        resp['code'] = -1
-        resp['msg'] = "请输入标签，便于搜索~~"
-        return jsonify(resp)
+    dish_info = d.PyFind_Name(dish_name)
+    dish_id = dish_info.DishID
 
-    food_info = Dish.query.filter_by(DishID=DishID).first()
-    if food_info:
-        model_food = food_info
+    # 以下表达暂定
+    if not dish_info:
+        r.PyAddDish(
+            dishname=dish_name,
+            price=dish_price,
+            type=dish_class,
+            description=dish_description
+        )
     else:
-        model_food = Dish()
-
-    model_food.DishType = dish_class
-    model_food.name = dish_name
-    model_food.Price = dish_price
-    model_food.Details_Picture = Details_Picture
-    model_food.Description = dish_description
-    model_food.DishTag = DishTag
-
-    db.session.add(model_food)
-    db.session.commit()
+        GenericModify(3, dish_id, Dish, ['DishName', 'Price', 'DishType', 'Description'], ['\'dish_name\'', \
+                                                                                           '\'dish_price\'',
+                                                                                           '\'dish-class\'',
+                                                                                           '\'dish_description\'', ])
 
     return jsonify(resp)
 
@@ -149,27 +105,16 @@ def set():
 # 餐品的删除
 @route_food.route("/ops", methods=["POST"])
 def ops():
-    resp = {'code': 200, 'msg': '操作成功~~', 'data': {}}
+    resp = {'code': 1, 'msg': '编辑成功', 'data': {}}
     req = request.values
 
-    DishID = req['DishID'] if 'DishID' in req else 0
-    act = req['act'] if 'act' in req else ''  # 前端传来的操作指令
+    dish_class = req['dish_class'] if 'dish_class' in req else ''  # 菜品分类
+    dish_name = req['dish_name'] if 'dish_name' in req else ''  # 菜品名
+    dish_price = req['dish_price'] if 'dish_price' in req else ''  # 菜品价格
+    dish_description = req['dish_description'] if 'dish_description' in req else ''  # 菜品描述
+    dish_info = d.PyFind_Name(dish_name)
+    dish_id = dish_info.DishID
 
-    if not id:
-        resp['code'] = -1
-        resp['msg'] = "请选择要操作的账号~~"
-        return jsonify(resp)
+    GenericModify(2, dish_id, Dish, 'dish_id')
 
-    food_info = Dish.query.filter_by(DishID=DishID).first()
-    if not food_info:
-        resp['code'] = -1
-        resp['msg'] = "指定美食不存在~~"
-        return jsonify(resp)
-
-    if act == "remove":
-        food_info.status = 0
-
-    # 直接删除该条记录（待改）
-    db.session.delete(food_info)
-    db.session.commit()
     return jsonify(resp)
--- backend/api/user/User.py
+++ backend/api/user/User.py
@@ -5,7 +5,7 @@ from utils.Helper import (ops_render)  # ops_render:渲染页面
 from utils.UrlManager import (UrlManager)  # UrlManager：统一封装生成各种url的方法
 from application import app, db
 import json
-from DataBaseFolder.Models.UserModels.UserBaseInfo import User # 导入数据库
+from DataBaseFolder.Interface.UserBaseModify import *  # 导入数据库修改接口
 
 route_user = Blueprint('user_page', __name__)
 
@@ -13,12 +13,9 @@ route_user = Blueprint('user_page', __name__)
 # 用户登录
 @route_user.route("/login", methods=["GET", "POST"])
 def login():
-    # 当请求为GET是，直接跳转到登录页面
-    if request.method == "GET":
-        return ops_render("user/login.html")
 
     # 当请求为POST
-    resp = {'code': 200, 'msg': '登录成功~~', 'data': {}}  # 返回信息
+    resp = {'code': 1, 'msg': '登录成功', 'data': {}}  # 返回信息
     req = request.values  # 前端传来的信息
 
     # 接口中使用的变量
@@ -27,28 +24,28 @@ def login():
 
     # 用户登录验证
     if login_name is None or len(login_name) < 1:  # 用户名未输入或太短
-        resp['code'] = -1
-        resp['msg'] = "请输入正确的登录用户名 -1~~"  # 测试，正式版本中去除"-1"，下同
+        resp['code'] = 0
+        resp['msg'] = "用户名或密码错误"
         return jsonify(resp)
 
     if login_pwd is None or len(login_pwd) < 1:  # 密码未输入或太短
-        resp['code'] = -1
-        resp['msg'] = "请输入正确的登录密码 -2~~"
+        resp['code'] = 0
+        resp['msg'] = "用户名或密码错误"
         return jsonify(resp)
 
-    user_info = User.query.filter_by(UserName=login_name).first()  # 从数据库寻找登录名相同的帐号
+    user_info = PyFind_Name(login_name)  # 从数据库寻找登录名相同的帐号
     if not user_info:
-        resp['code'] = -1
-        resp['msg'] = "请输入正确的登录用户名和密码 -3~~"
+        resp['code'] = 0
+        resp['msg'] = "用户名或密码错误"
         return jsonify(resp)
 
     if user_info._Password != login_pwd:
-        resp['code'] = -1
-        resp['msg'] = "请输入正确的登录用户名和密码 -4~~"
+        resp['code'] = 0
+        resp['msg'] = "用户名或密码错误"
         return jsonify(resp)
 
     # uid：用户对应的编号
-    response = make_response(json.dumps({'code': 200, 'msg': '登录成功~~'}))  # 返回登录成功的信息
+    response = make_response(json.dumps({'code': 1, 'msg': '登录成功'}))  # 返回登录成功的信息
     # 生成cookie
     response.set_cookie(app.config['AUTH_COOKIE_NAME'], '%s#%s' % (
         UserService.geneAuthCode(user_info), user_info.UserID), 60 * 60 * 24 * 120)  # 保存120天
