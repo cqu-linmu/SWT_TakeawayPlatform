ce1e191de6b951661baea77ba8ebae5f33d53024
Aression <1040901628@qq.com>
2021-08-24 16:44:35 +0800

    更新dishList和orderList

--- backend/api/dishList.py
+++ backend/api/dishList.py
@@ -5,33 +5,54 @@ from application import app, db
 from utils.UrlManager import UrlManager
 from decimal import Decimal
 from sqlalchemy import or_
+
 # 相关数据库调用
 from DataBaseFolder.Interface import DishBaseModify as d
 from DataBaseFolder.Interface import RestaurantBaseModify as r
 from DataBaseFolder.Interface.InterfaceHelper import *
 
-
 route_dishList = Blueprint('dish-list', __name__)
 
-@route_dishList.route("/", methods=['GET'])
+
+@route_dishList.route("/", methods=['GET', 'POST'])
 def index():
-    
+    # 获取请求结果
     req = request.values
-    pageNum = int(req['page']) if ('page' in req and req['page']) else 1
+
+    # 解析参数
+    pageNum = int(req['pageNum']) if ('pageNum' in req and req['pageNum']) else 1
     page_size = int(req['pageSize'])
+
+    # 从数据库拉取列表信息
     dishList = d.PyList()
     listNum = len(dishList)
     dishList = dishList[(pageNum - 1) * page_size:pageNum * page_size]
-    lic = []
 
+    # 数据为空时查询失败
+    if listNum < 1 or pageNum > listNum:
+        lic = {
+            'code': 400,
+            'message': '请求失败,检查当前页数或者联系后台检查数据库',
+            'data': []
+        }
+        return jsonify(lic)
+
+    # 数据有效时，创建响应体
+    lic = {
+        'code': 200,
+        'message': '请求成功',
+        'data': [],
+        'total': listNum
+    }
+
+    # 加入菜品数据
     def bedict(dataList):
         for item in dataList:
-            lic.append(
+            lic['data'].append(
                 {
                     "dish_id": item.DishID,
                     "dish_name": item.DishName,
                     "dish_class": item.DishType
-
                 }
             )
         return lic
--- backend/api/orderList.py
+++ backend/api/orderList.py
@@ -1,30 +1,53 @@
 from flask import Blueprint, request, jsonify, redirect
-from DataBaseFolder.Models.OrderData.OrderBase import Order 
+from DataBaseFolder.Models.OrderData.OrderBase import Order
 from DataBaseFolder.Interface import OrderBaseModify as o
 from utils.Helper import *
 from utils.UrlManager import UrlManager
 from application import app, db
 import json
 
-route_orderList= Blueprint('order-list', __name__)
+route_orderList = Blueprint('order-list', __name__)
 
 
-# 订单详情页面接口 [接口7]
-@route_orderList.route("/", methods=['POST'])
+# 订单详情页面接口 [接口7] 
+@route_orderList.route("/", methods=['POST', 'GET'])
 def index():
+    # 获取请求结果
     req = request.values
-    page = int(req['page']) if ('page' in req and req['page']) else 1
-    
-    page_size = int(req['pageSize'])
-    orderList =o.PyList()
-    listNum=len(orderList)
-    pageNum=listNum/page_size
-    orderList=orderList[(page-1)*page_size:page*page_size]
+
+    # 解析参数
+    pageNum = int(req['pageNum']) if ('pageNum' in req and req['pageNum']) else 1
+    pageSize = int(req['pageSize'])
+
+    # 查询数据库订单数据
+    orderList = o.PyList()
+
+    # 计算分页情况
+    listNum = len(orderList)
+    orderList = orderList[(pageNum - 1) * pageSize:pageNum * pageSize]
     lic = []
-    
-    def bedict(a):
-        for item in a:
-            lic.append(
+
+    # 数据为空时查询失败
+    if listNum < 1 or pageNum > listNum:
+        lic = {
+            'code': 400,
+            'message': '请求失败,检查当前页数或者联系后台检查数据库',
+            'data': []
+        }
+        return jsonify(lic)
+
+    # 数据有效时，创建响应体
+    lic = {
+        'code': 200,
+        'message': '请求成功',
+        'data': [],
+        'total': listNum
+    }
+
+    # 加入菜品数据
+    def bedict(dataList):
+        for item in dataList:
+            lic['data'].append(
                 {
                     "order_dish": item.Dishes,
                     "order_id": item.OrderID,
@@ -34,6 +57,5 @@ def index():
                 }
             )
         return lic
-    
-    # 不用返回总页数
-    return jsonify(bedict(orderList))  
+
+    return jsonify(bedict(orderList))
