7b6dea2d0b8508cb145e548ad5adbbdaa514d3f8
handsomefan1 <2642413273@qq.com>
2021-08-24 12:30:58 +0800

    测试后修改版本

--- backend/api/interceptors/AuthInterceptor.py
+++ backend/api/interceptors/AuthInterceptor.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# 统一拦截器，避免未登录直接进入主页面
 from application import app
 from flask import request, g, redirect
-
-from DataBaseFolder.Models.UserModels.UserBaseInfo import User
-from common.libs.user.UserService import (UserService)
-from common.libs.UrlManager import (UrlManager)
+# from common.models.User import (UserdataAll)
+from DataBaseFolder.Interface.UserBaseModify import *
+from utils.user.UserService import (UserService)
+from utils.UrlManager import (UrlManager)
 import re
 
 
@@ -15,25 +14,28 @@ def before_request():
     ignore_check_login_urls = app.config['IGNORE_CHECK_LOGIN_URLS']
     path = request.path
 
+    app.logger.info
     # 如果是静态文件就不要查询用户信息了
     pattern = re.compile('%s' % "|".join(ignore_check_login_urls))
     if pattern.match(path):
         return
 
+    user_info = check_login()
+
+    app.logger.info(user_info)
+
+    g.current_user = None
+
+    if user_info:
+        g.current_user = user_info
+
     pattern = re.compile('%s' % "|".join(ignore_urls))
     if pattern.match(path):
         return
 
-    if '/api' in path:
-        return
-
-    user_info = check_login()
-    g.current_user = None
-    if user_info:
-        g.current_user = user_info
-
     if not user_info:
         return redirect(UrlManager.buildUrl("/user/login"))
+
 
     return
 
@@ -53,7 +55,7 @@ def check_login():
         return False
 
     try:
-        user_info = User.query.filter_by(UserID=auth_info[1]).first()
+        user_info = PyFind_ID(auth_info[1])
     except Exception:
         return False
 
@@ -63,7 +65,5 @@ def check_login():
     if auth_info[0] != UserService.geneAuthCode(user_info):
         return False
 
-    if user_info.status != 1:
-        return False
 
     return user_info
--- backend/api/user/User.py
+++ backend/api/user/User.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, request, jsonify, make_response, redirect
-from flask import g
+from flask import Blueprint, request, g, jsonify, make_response, redirect
+from common.libs.user.UserService import ( UserService )
 from utils.UrlManager import (UrlManager)  # UrlManager：统一封装生成各种url的方法
 from application import app, db
 import json
@@ -11,7 +11,7 @@ route_user = Blueprint('user_page', __name__)
 
 
 # 用户登录
-@route_user.route("api/login", methods=["POST"])
+@route_user.route("/login", methods=["POST"])
 def login():
     # 当请求为POST
     resp = {'code': 200, 'message': '登录成功', 'data': {}}  # 返回信息
@@ -24,12 +24,12 @@ def login():
     user_info = PyFind_Name(userName)  # 从数据库寻找登录名相同的帐号
     if not user_info:
         resp['code'] = 400
-        resp['message'] = "用户名或密码错误"
+        resp['message'] = "用户名或密码错误-1"
         return jsonify(resp)
 
     if user_info._Password != password:
         resp['code'] = 400
-        resp['message'] = "用户名或密码错误"
+        resp['message'] = "用户名或密码错误-2"
         return jsonify(resp)
 
     token = random.randint(1, 99999)
@@ -38,12 +38,13 @@ def login():
 
     response = make_response(
         json.dumps({'code': 200, 'msg': '登录成功', 'data': {'token': token, 'refresh_token': refresh_token}}))  # 返回登录成功的信息
-
+    response.set_cookie(app.config['AUTH_COOKIE_NAME'], '%s#%s' % (
+        UserService.geneAuthCode(user_info), user_info.UserID), 60 * 60 * 24 * 120)  # 保存120天
     return response
 
 
 # 登录用户的信息
-@route_user.route("api/userinfo", methods=["get"])
+@route_user.route("/userinfo", methods=["GET"])
 def userinfo():
     resp = {'code': 200, 'message': '获取用户信息成功', 'data': {}}  # 返回信息
     if g.current_user:
