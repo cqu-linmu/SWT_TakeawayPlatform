5172d3dd3ca1e78269febe9f9d128a7a7a1d2e0a
handsomefan1 <2642413273@qq.com>
2021-08-29 22:53:14 +0800

    测试通过地址查询与更新

--- backend/api/WXSearchAddress.py
+++ backend/api/WXSearchAddress.py
@@ -4,3 +4,27 @@ from flask import Blueprint, request, jsonify
 import DataBaseFolder.Interface.UserBaseModify as U
 
 route_WXSearchAddress = Blueprint('WXSearchAddress', __name__)
+
+
+@route_WXSearchAddress.route("/", methods=["POST", "GET"])
+def searchAddress():
+    """
+    查询收获地址
+    """
+    resp = {'code': 200, 'message': '操作成功', 'data': {}}
+    req = request.values
+    user_id = req['user_id'] if 'user_id' in req else ' '  # 登录用户id
+
+    # 查询用户信息
+    member_info = U.PyFind_ID(user_id)
+    member_address = member_info.Address
+
+    # 地址不存在
+    if not member_address:
+        resp['code'] = 400
+        resp['message'] = "不存在地址"
+        return jsonify(resp)
+    resp['data'] = {
+        'user_address': member_address
+    }
+    return jsonify(resp)
